96c7b09aa4395cc306af0f9a97f0b73bc4cda714
Colin Ries <colin.ries@gmail.com>
2024-08-05 17:34:52 +0200

    use serial to write config

--- webapp/serial_interface.py
+++ webapp/serial_interface.py
@@ -2,6 +2,35 @@ import json
 
 from config_manager import read_effect_config
 from forms import setup_config
+from time import sleep
+
+import serial
+
+
+connected = False
+attempts_left = 60
+while not connected and attempts_left > 1:
+    try:
+        ser = serial.Serial('/dev/serial0', 9600, timeout=2)
+        ser.flush()
+        connected = True
+        print("connected!")
+    except serial.serialutil.SerialException:
+        sleep(5)
+        attempts_left -= 1
+        pass
+
+if not connected:
+    exit(1)
+
+
+def write_to_serial(json_dict):
+    line = (json.dumps(json_dict) + "\n").encode('utf-8')
+    ser.write(line)
+    print("written:", line)
+    if ser.in_waiting:
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
 
 
 def send_config_factory():
@@ -20,7 +49,7 @@ def send_config_factory():
 
                 if len(data_to_send) > 0:
                     json_to_send = {"target": "element", "index": element_index, "type": config_type, "data": data_to_send}
-                    print(json.dumps(json_to_send))
+                    write_to_serial(json_to_send)
 
         old_config = new_effect_config
 
@@ -35,4 +64,4 @@ def send_entire_config():
             data_to_send = effect_config["elements"][element_index][config_type]
 
             json_to_send = {"target": "element", "index": element_index, "type": config_type, "data": data_to_send}
-            print(json.dumps(json_to_send))
+            write_to_serial(json_to_send)
